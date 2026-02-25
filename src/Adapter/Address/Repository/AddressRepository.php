7ae4748e4873f8e77b4bb9e0e862c86a6ad0d707
soledis_caroline_lesle <caroline.lesle@soledis.com>
2026-02-25 09:07:59 +0100

    New creation form for business entity
    fix after github action
    
    refactor: update address management in BusinessEntity forms and improve data handling
    
    add NewAddress DTO and adapt BusinessEntity handling for address management
    
    set country/state fields and data handling in business entity forms
    
    add ts function to manage postcode/state in business entity address form
    
    set licenses
    
    Set address type for business entity and fix code
    
    Add typescript to manage business entity default address bg color
    
    Add typescript to manage business entity default address
    
    wip - return BusinessEntityId from AddBusinessEntityHandler
    
    wip - add address to business entity using cqrs
    
    wip - start business entity cqrs implementation
    
    wip - init createAction for BusinessEntity
    
    refactor b2b tabs management
    
    B2B: toggle Business Entities tabs based on improved_b2b flag and shop mode

--- src/Adapter/Address/Repository/AddressRepository.php
+++ src/Adapter/Address/Repository/AddressRepository.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 namespace PrestaShop\PrestaShop\Adapter\Address\Repository;
 
 use Address;
+use PrestaShop\PrestaShop\Adapter\Address\DTO\NewAddress;
 use PrestaShop\PrestaShop\Core\Domain\Address\Exception\AddressNotFoundException;
 use PrestaShop\PrestaShop\Core\Domain\Address\ValueObject\AddressId;
 use PrestaShop\PrestaShop\Core\Domain\AttributeGroup\Attribute\Exception\AttributeNotFoundException;
@@ -38,5 +39,53 @@ class AddressRepository extends AbstractMultiShopObjectModelRepository
         );
 
         return $address;
+    }
+
+    /**
+     * @throws CoreException
+     */
+    public function add(NewAddress $address): AddressId
+    {
+        $addressModel = new Address();
+
+        // required fields
+        $addressModel->alias = $address->getAlias();
+        $addressModel->address1 = $address->getAddress1();
+        $addressModel->lastname = $address->getLastName();
+        $addressModel->firstname = $address->getFirstName();
+        $addressModel->city = $address->getCity();
+        $addressModel->postcode = $address->getPostcode();
+        $addressModel->id_country = $address->getCountryId()->getValue();
+
+        // optional fields
+        $addressModel->id_state = $address->getStateId();
+        $addressModel->id_customer = $address->getCustomerId();
+        $addressModel->id_manufacturer = $address->getManufacturerId();
+        $addressModel->id_supplier = $address->getSupplierId();
+        $addressModel->address2 = $address->getAddress2();
+        $addressModel->phone = $address->getPhone();
+        $addressModel->phone_mobile = $address->getPhoneMobile();
+        $addressModel->dni = $address->getDni();
+        $addressModel->company = $address->getCompany();
+        $addressModel->vat_number = $address->getVatNumber();
+        $addressModel->other = $address->getOther();
+
+        $addressModel->deleted = $address->isDeleted();
+
+        $addressId = $this->addObjectModel($addressModel, Address::class);
+
+        return new AddressId($addressId);
+    }
+
+    /**
+     * @throws CoreException
+     * @throws AttributeNotFoundException
+     */
+    public function delete(AddressId $addressId): void
+    {
+        $this->deleteObjectModel(
+            $this->get($addressId),
+            Address::class
+        );
     }
 }
